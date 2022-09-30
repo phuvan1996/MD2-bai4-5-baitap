899423e28aad2d368915ac895cdbc333cef36e1e
Phạm Phú Văn <phuvan2604@gmail.com>
2022-09-30 16:07:33 +0700

    MD2-BAI5-BAI TAP 2

---
+++ src/MD2_BAI5_BAITAP2/Student.java
@@ -0,0 +1,22 @@
+package MD2_BAI5_BAITAP2;
+
+public class Student {
+    private String name = "John";
+    private  String classes = "CO2";
+
+    public Student() {
+    }
+
+    public Student(String name, String classes) {
+        this.name = name;
+        this.classes = classes;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ src/MD2_BAI5_BAITAP2/Test.java
@@ -0,0 +1,12 @@
+package MD2_BAI5_BAITAP2;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Student s1 = new Student();
+        s1.setName(scanner.nextLine());
+        s1.setClasses(scanner.nextLine());
+    }
+}
